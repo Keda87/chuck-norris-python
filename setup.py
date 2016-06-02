cec9be3d7a18eceddf450139d47c6a3bfab69302
adiyatmubarak <adiyatmubarak@gmail.com>
2016-06-02 19:33:58 +0700

    feat: add pypi setup file

---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+
+setup(
+    name='chuck-norris-python',
+    version='0.3',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    license='MIT',
+    long_description='Python wrapper for "The Internet Chuck Norris Database" http://www.icndb.com/api/',
+    install_requires=['requests'],
+    url='https://github.com/Keda87/chuck-norris-python',
+    author='Adiyat Mubarak',
+    author_email='adiyatmubarak@gmail.com'
+)
